5982d72ce2a5d6a4977400316a379e217a0689a8
skyman2413 <stepan26062001@gmail.com>
2023-02-27 02:42:29 +0300

    fixed Path

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 # coding: utf8
 import asyncio
 import logging
+import pathlib
+
 from aiogram import Bot, Dispatcher
 from config_reader import config
 from handlers import root_handler, book_info, faq_handler, services_handler, unhandled
 
 
 async def main():
-    logging.basicConfig(level=logging.DEBUG)
+    path = str(pathlib.Path(pathlib.Path.cwd(), "logs", "log.txt"))
+    logging.basicConfig(level=logging.INFO, filename=path)
     bot = Bot(token=config.bot_token.get_secret_value())
     dp = Dispatcher()
     dp.include_router(root_handler.router)
